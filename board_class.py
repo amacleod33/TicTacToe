6e5ff2c9e9775ddbe1996cadf96c803a61535776
Alexander MacLeod <alexmacleod@Alexanders-MacBook-Pro.local>
2018-11-28 21:15:04 -0500

    Updated p4 with comments

--- board_class.py
+++ board_class.py
@@ -1,12 +1,12 @@
 class Board:
     SIZE = 9
     BLANK = 'N'
-
+#initialize constand board, spots left, and board
     def __init__(self):
         self.constant_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.spots_left = self.constant_board
         self.board = ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N']
-
+#function returns the number of spots left
     def nbr_of_unfilled_slots(self):
         s = 0
         for i in self.spots_left:
@@ -14,12 +14,15 @@ class Board:
                 s = s + 1
         return s
 
+#function takes removes taken from spots_left list
     def take_spot(self, taken):
         return self.spots_left.remove(taken)
 
+#function returns index of spots_left
     def choose_spot_at(self, index):
         return self.spots_left[index]
 
+#creates a display of the tic tac toe board
     def display(self):
         s = 0
         for i in range(3):
@@ -29,6 +32,7 @@ class Board:
             print('')
         return
 
+#returns true or false depending on if the index is in spots_left
     def is_available(self, index):
         for i in self.spots_left:
             if index in self.spots_left:
@@ -36,6 +40,7 @@ class Board:
             else:
                 return False
 
+#puts x in indicated index in board and removed index from spots_left
     def setX(self, index):
         if index in self.spots_left:
             self.board.remove(self.board[index-1])
@@ -45,6 +50,7 @@ class Board:
             return None
         return
 
+#puts o in indicated index in board and removes index from spots_left
     def setO(self, index):
         if index in self.spots_left:
             self.board.remove(self.board[index-1])
@@ -54,6 +60,7 @@ class Board:
             return None
         return
 
+#determines if there is a winner
     def win(self):
         if self.board[0] == 'O' and self.board[1] == 'O' and self.board[2] == 'O' or self.board[0] == 'X' and self.board[1] == 'X' and self.board[2] == 'X':
             return True
@@ -74,6 +81,7 @@ class Board:
         else:
             return False
 
+#returns the winning letter
     def get_winner(self):
         if self.board[0] == 'X' and self.board[1] == 'X' and self.board[2] == 'X':
             return 'X'
@@ -110,6 +118,7 @@ class Board:
         else:
             return 'N'
 
+#determines if the board is full
     def full(self):
         if len(self.spots_left) == 0:
             return True
